b636365961e6b1deca6061c0b36a29b42da81789
uddeshyac9 <uddeshyac9@gmail.com>
2024-04-30 11:16:22 +0530

    layout-page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+
+
+
+function AboutPage() {
+
+
+    return (
+        <div>
+            <h1 className="text-3xl text-center">About Page</h1>
+        </div>
+    )
+}
+
+export default AboutPage
